7d013c1a96174e674c25c3b06f3c8ce41c3c5cfe
Rostyslav <malanyukrostik@gmail.com>
2022-04-08 19:04:04 +0300

    add token authorization

--- src/config/settings.py
+++ src/config/settings.py
@@ -28,6 +28,9 @@ INSTALLED_APPS = [
 
     'core',
     'rest_framework',
+
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -112,3 +115,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+} 
